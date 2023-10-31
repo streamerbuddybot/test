13a7ae535d61c8e1c94ed9fd0fdbbe4970699d47
Jochemwhite <j.vanderwit@amrio.nl>
2023-10-31 02:09:28 -0400

    dsfas

--- src/app/dashboard/loading.tsx
+++
@@ -1,9 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex h-dashboard w-full select-none flex-col items-center justify-center gap-2 rounded-md border border-dashed border-border bg-border/10">
-      <p className="text-sm text-muted-foreground">
-        Loading....
-      </p>
-    </div>
-  )
-}
